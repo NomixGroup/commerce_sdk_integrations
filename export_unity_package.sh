7b79b6f88cbaba7d731dcccd9814cf13c4bec924
narcisbadea1 <narcisbadea@gmail.com>
2025-01-22 16:29:37 +0200

    sdk 1.3

--- export_unity_package.sh
+++ export_unity_package.sh
@@ -5,10 +5,10 @@ set -e
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Update this path to your Unity executable
-UNITY_PATH="/Applications/Unity/Hub/Editor/2022.3.38f1/Unity.app/Contents/MacOS/Unity"
+UNITY_PATH="/Applications/Unity/Hub/Editor/2022.3.54f1/Unity.app/Contents/MacOS/Unity"
 
 PROJECT_PATH="$SCRIPT_DIR/Unity"  # path to Unity project
-PACKAGE_NAME="unity-1.2.0.unitypackage"  # name of the package
+PACKAGE_NAME="unity-1.3.unitypackage"  # name of the package
 EXPORT_PATH="$SCRIPT_DIR/build"     # Set the directory to export the package
 
 # Asset folders (one per line)
