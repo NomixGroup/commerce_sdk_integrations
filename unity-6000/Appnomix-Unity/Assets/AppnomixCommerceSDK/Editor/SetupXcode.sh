28f33f1dfee0161e4e95b65bdb199d9a73527b50
narcisbadea1 <narcisbadea@gmail.com>
2025-09-04 19:42:20 +0300

    update iOS implementation for CSDK 2.0

--- unity-6000/Appnomix-Unity/Assets/AppnomixCommerceSDK/Editor/SetupXcode.sh
+++ unity-6000/Appnomix-Unity/Assets/AppnomixCommerceSDK/Editor/SetupXcode.sh
@@ -16,7 +16,7 @@ PROJECT_PATH="$1"
 XC_TEMPLATE_NAME="Appnomix.Safari.Extension.xctemplate"
 XC_FRAMEWORK_NAME="AppnomixCommerce.xcframework"
 
-XC_VERSION=1.7.3
+XC_VERSION=2.0.0-beta02
 
 TEMPLATE_URL="https://github.com/NomixGroup/ios_commerce_sdk_binary/releases/download/$XC_VERSION/$XC_TEMPLATE_NAME.zip"
 SWIFT_PACKAGE_URL="https://github.com/NomixGroup/ios_commerce_sdk_binary"
