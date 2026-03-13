fe7969570999ed0c5be295ee3d479dcf4379396f
Narcis Badea <157131823+narcisbadea1@users.noreply.github.com>
2026-03-13 16:30:45 +0200

    Release/v2.1.0 (#21)

--- unity-6000/Appnomix-Unity/Assets/AppnomixCommerceSDK/Editor/SetupXcode.sh
+++ unity-6000/Appnomix-Unity/Assets/AppnomixCommerceSDK/Editor/SetupXcode.sh
@@ -16,7 +16,7 @@ PROJECT_PATH="$1"
 XC_TEMPLATE_NAME="Appnomix.Safari.Extension.xctemplate"
 XC_FRAMEWORK_NAME="AppnomixCommerce.xcframework"
 
-XC_VERSION=2.0.0
+XC_VERSION=2.1.0
 
 TEMPLATE_URL="https://github.com/NomixGroup/ios_commerce_sdk_binary/releases/download/$XC_VERSION/$XC_TEMPLATE_NAME.zip"
 SWIFT_PACKAGE_URL="https://github.com/NomixGroup/ios_commerce_sdk_binary"
